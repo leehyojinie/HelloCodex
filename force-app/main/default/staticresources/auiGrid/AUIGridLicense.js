c35f5e7b08dd99246042effa8028e6a306309a9b
jinie <leehyojinie@gmail.com>
2025-08-22 14:51:08 +0900

    Initial commit

---
+++ force-app/main/default/staticresources/auiGrid/AUIGridLicense.js
@@ -0,0 +1,10 @@
+/*!
+ * AUIGrid v3.0.16 License
+ * License Type : Evaluation(Trial) Version
+ * Authorized Domain(or IP) : localhost,127.0.0.1,hyo-dev-ed--c.develop.vf.force.com,hyo-dev-ed.develop.lightning.force.com,hyo-dev-ed.develop.my.salesforce.com,hyo-dev-ed.develop.my.site.com
+ * Expiration Date : 2025-08-06
+ * www.auisoft.net
+ */
+ /* eslint-disable */
+const AUIGridLicense = "eyJjdCI6Im5DN2R5bkwyWXVLeVpyV3hkdTJXZHVMbWYwcG9qeVRUc1JmQ2loM2pJT0J2RE4rcUdWeUR6YXVjRHRzWWFaY1wvRGpMWmZJcUhBWHpwUVdicGFYQjZIeXRsT1JiTms0bzNUSTlpbmY2ZzE0TVVcL0w2MVRcL3E5bVBDNmhkeDk4WWpxdXptSUgzRzRnU3UrMWN4VVMyNXBiN3BwN2ZpRDFpak9EbWR6TitOMkNvem1xM2FmYkdSYkFUcFJqMkR4YXZBdTdtenhlMmxzZFkyZzlocXBcL1NrYWRlWlVIbkl1XC83ZXhhbDI1bThSWGZsMnJPM1wvV1FXNGJzaHFkWU1GYldxK3ZxU1lsSkpJdTVxcnlVZHRlTGMwYmRRPT0iLCJpdiI6IjU0MzA5NGRhYWU2Y2Q0YTgwNWMwMmQ1MGZjY2M5NDJiIiwicyI6IjgyODBjYWI5ODYxZGQ3NzEifQ==";
+if (typeof window !== "undefined") window.AUIGridLicense = AUIGridLicense;
